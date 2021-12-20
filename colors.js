d41505489dde2e66e0324568262e4c6e840eb15b
Janus Hasseriis <janus@hasseriis.net>
2021-12-20 22:43:30 +0100

    Added colors

---
+++ colors.js
@@ -0,0 +1,23 @@
+/* Extended Array */
+const colors = [
+  {
+    name: "Charcoal",
+    rgb: [38, 70, 83, 255],
+  },
+  {
+    name: "Persian Green",
+    rgb: [42, 157, 143, 255],
+  },
+  {
+    name: "Orange Yellow Crayola",
+    rgb: [233, 196, 106, 255],
+  },
+  {
+    name: "Sandy Brown",
+    rgb: [244, 162, 97, 255],
+  },
+  {
+    name: "Burnt Sienna",
+    rgb: [231, 111, 81, 255],
+  },
+];
