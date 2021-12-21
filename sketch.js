b6ba68a637632976ea7fe57b325adf303dd648ea
Janus Hasseriis <janus@hasseriis.net>
2021-12-21 23:08:28 +0100

    Negative margin

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 let size = 800;
 //let dimFactor = Math.floor(Math.random() * 100);
-let dimFactor = 10;
-let strokeWidth = 2;
-let ifMargin = true;
-let margin = ifMargin ? dimFactor + strokeWidth + (size % dimFactor) : 0;
+let dimFactor = 50;
+let strokeWidth = 44;
+let ifMargin = false;
+let marginVal = 0.5 * dimFactor + strokeWidth + (size % dimFactor);
+let margin = ifMargin ? marginVal : -marginVal;
 const palette = Math.floor(Math.random() * colors.length);
 
 const pairs = [[20, 30, "PROJECT"]];
@@ -22,23 +23,23 @@ function getRandomInt() {
 
 function draw() {
   // Draw horizontal lines
-  for (let hPos = margin; hPos < size - margin + 5; hPos += 2 * dimFactor) {
+  for (let hPos = margin; hPos < size - margin + 5; hPos += dimFactor) {
     for (
-      let pos = margin + getRandomInt() * (2 * dimFactor);
+      let pos = margin + getRandomInt() * dimFactor;
       pos < size - margin;
-      pos += 4 * dimFactor
+      pos += 2 * dimFactor
     ) {
-      line(pos, hPos, pos + 2 * dimFactor, hPos);
+      line(pos, hPos, pos + dimFactor, hPos);
     }
   }
   // Draw vertical lines
-  for (let vPos = margin; vPos < size - margin + 5; vPos += 2 * dimFactor) {
+  for (let vPos = margin; vPos < size - margin + 5; vPos += dimFactor) {
     for (
-      let pos = margin + getRandomInt() * (2 * dimFactor);
+      let pos = margin + getRandomInt() * dimFactor;
       pos < size - margin;
-      pos += 4 * dimFactor
+      pos += 2 * dimFactor
     ) {
-      line(vPos, pos, vPos, pos + 2 * dimFactor);
+      line(vPos, pos, vPos, pos + dimFactor);
     }
   }
   noLoop();
